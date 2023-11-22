e1668c9045647cc0aad91b553f40634ff5e640dd
DrakeAHoffman <drakehoffman99@gmail.com>
2023-11-21 19:09:27 -0800

    finished assignment

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,16 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express();
 
+  
+app.use('/places', require('./controllers/places'))
+app.use('/users', require('./controllers/users'))
+app.use('/authentication', require('./controllers/authentication'))
+
+// Listen for Connections
+app.listen(process.env.PORT, () => {
+  
+}),
+
 // Express Settings
 app.use(cors())
 app.use(express.static('public'))
